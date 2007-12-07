0fb2be804641d5969b6c987fb96d15f51b10acd4
Elvio Basello <hellvis69@gmail.com>
2007-12-07 18:46:52 +0000

    quick fix
    git-svn-id: https://svn.kvirc.de/svn/branches/kvirc/qmake@989 17fca916-40b9-46aa-a4ea-0a15b648b75c

---
+++ src/modules/mediaplayer/mp_audaciousinterface.h
@@ -0,0 +1,66 @@
+#ifndef _MP_AUDACIOUSINTERFACE_H_
+#define _MP_AUDACIOUSINTERFACE_H_
+//=============================================================================
+//
+//   File : mp_audaciousinterface.h
+//   Created on Thu 06 Dec 2007 14:20:02 by Tomasz Moń
+//
+//   This file is part of the KVIrc IRC client distribution
+//   Copyright (C) 2007 Tomasz Moń <desowin@gmail.com>
+//
+//   This program is FREE software. You can redistribute it and/or
+//   modify it under the terms of the GNU General Public License
+//   as published by the Free Software Foundation; either version 2
+//   of the License, or (at your opinion) any later version.
+//
+//   This program is distributed in the HOPE that it will be USEFUL,
+//   but WITHOUT ANY WARRANTY; without even the implied warranty of
+//   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+//   See the GNU General Public License for more details.
+//
+//   You should have received a copy of the GNU General Public License
+//   along with this program. If not, write to the Free Software Foundation,
+//   Inc. ,59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+//
+//=============================================================================
+
+#include "kvi_settings.h"
+#include "mp_interface.h"
+#include "kvi_library.h"
+
+#ifndef COMPILE_ON_WINDOWS
+#include <QtDBus/QtDBus>
+
+	class KviAudaciousInterface : public KviMediaPlayerInterface
+	{
+	public:
+		KviAudaciousInterface();
+		virtual ~KviAudaciousInterface();
+	public:
+		virtual int detect(bool bStart);
+		virtual bool prev();
+		virtual bool next();
+		virtual bool play();
+		virtual bool stop();
+		virtual bool pause();
+		virtual bool quit();
+		virtual QString nowPlaying();
+		virtual QString mrl();
+		virtual KviMediaPlayerInterface::PlayerStatus status();
+		virtual int length();
+		virtual QString title();
+		virtual QString artist();
+		virtual QString genre();
+		virtual QString comment();
+		virtual QString album();
+
+		virtual int getPlayListPos();
+		virtual QString year();
+		virtual QString mediaType();
+	};
+	
+	MP_DECLARE_DESCRIPTOR(KviAudaciousInterface)
+	
+#endif //!COMPILE_ON_WINDOWS
+
+#endif //!_MP_AUDACIOUSINTERFACE_H_
