d097985d425ff633b78e15a37e8ccf6f05342cda
Alexey Uzhva <alexey@uzhva.ru>
2007-12-15 18:50:11 +0000

    removed unnesessary __KVILIB__
    git-svn-id: https://svn.kvirc.de/svn/branches/kvirc/qmake@1011 17fca916-40b9-46aa-a4ea-0a15b648b75c

--- src/kvilib/net/kvi_dns.cpp
+++ src/kvilib/net/kvi_dns.cpp
@@ -21,7 +21,6 @@
 //   Inc. ,59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
 //
 //=============================================================================
-#define __KVILIB__
 
 #include "kvi_dns.h"
 #include "kvi_error.h"
